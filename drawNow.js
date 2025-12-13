7b29b814db92ac96a974eb521591dd0f8f4de62e
unknown <shivishalsharma@gmail.com>
2025-12-14 01:57:53 +0530

    color-stroke UI

--- drawNow.js
+++ drawNow.js
@@ -1,10 +1,49 @@
 const leftLiTools = document.querySelectorAll(".left ul li");
+const clors = document.querySelector(".diffClr");
+const widths = document.querySelector(".widths ul");
 
 leftLiTools.forEach((li) => {
   li.addEventListener("click", () => {
-    // e.style.classList.remove;
-    leftLiTools.forEach((item) => item.classList.remove("acitve"));
+    leftLiTools.forEach((item) => {
+      item.classList.remove("active");
+    });
+
     li.classList.add("active");
-    // console.log(e.currentTarget.);
   });
 });
+
+function colorMaker() {
+  const clrs = [
+    "black",
+    "orange",
+    "red",
+    "yellow",
+    "green",
+    "skyblue",
+    "blue",
+    "purple",
+    "pink",
+    "white",
+  ];
+  clrs.forEach((i) => {
+    const li = document.createElement("li");
+    li.style.backgroundColor = i;
+    clors.append(li);
+    console.log(i);
+  });
+}
+
+function widthMaker() {
+  for (let i = 4; i <= 36; i = i * 1.5) {
+    const li = document.createElement("li");
+    // li.textContent = ".";
+    li.style.width = `${i}px`;
+    li.style.height = `${i}px`;
+    li.style.backgroundColor = "rgba(128, 128, 128, 0.722)";
+    li.style.borderRadius = "50%";
+    widths.append(li);
+  }
+}
+
+widthMaker();
+colorMaker();
